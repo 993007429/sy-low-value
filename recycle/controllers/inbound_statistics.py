4ff0b7661f6dc4fa8843a161691054bc6dc86f50
zhuangbaojun <zhuangbaojun@ztbory.com>
2022-06-30 19:47:19 +0800

    feat: 增加按照中转站统计处理量接口

--- recycle/controllers/inbound_statistics.py
+++ recycle/controllers/inbound_statistics.py
@@ -1,16 +1,17 @@
 from datetime import date, timedelta
+from typing import List
 
-from django.db.models import Sum
+from django.db.models import Count, F, Sum
 from ninja import Router
 
 from recycle.models import InboundRecord
-from recycle.schemas.inbound_statistics import ThroughputOut
+from recycle.schemas.inbound_statistics import ThroughputByStationOut, ThroughputOut
 
 router = Router(tags=["进场统计"])
 
 
 @router.get("/throughput", response=ThroughputOut)
-def throughput(request, start_date: date, end_date: date, street_code: str = None):
+def calc_throughput(request, start_date: date, end_date: date, street_code: str = None):
     """进场可回收物处理量"""
 
     end_date = end_date + timedelta(days=1)
@@ -20,4 +21,25 @@ def throughput(request, start_date: date, end_date: date, street_code: str = Non
     aggregations = queryset.aggregate(throughput=Sum("net_weight"))
     if not aggregations["throughput"]:  # sum没有值时会返回None
         aggregations["throughput"] = 0
+    return aggregations
+
+
+@router.get("/throughput_by_station", response=List[ThroughputByStationOut])
+def calc_throughput_by_station(request, start_date: date, end_date: date, street_code: str = None):
+    """按照中转站统计进场量"""
+
+    end_date = end_date + timedelta(days=1)
+    queryset = InboundRecord.objects.filter(net_weight_time__gte=start_date, net_weight_time__lt=end_date)
+    if street_code:
+        queryset = queryset.filter(station__street__code=street_code)
+    aggregations = (
+        queryset.annotate(station_name=F("station__name"))
+        .values("station_id", "station_name")
+        .annotate(throughput=Sum("net_weight"), count=Count("*"))
+        .order_by("-throughput")
+    )
+    for agg in aggregations:
+        if not agg["throughput"]:  # sum没有值时会返回None
+            agg["throughput"] = 0
+
     return aggregations
--- recycle/schemas/inbound_statistics.py
+++ recycle/schemas/inbound_statistics.py
@@ -4,3 +4,10 @@ from pydantic import Field
 
 class ThroughputOut(Schema):
     throughput: float = Field(0, title="处理量")
+
+
+class ThroughputByStationOut(Schema):
+    station_id: str = Field(title="中转站id")
+    station_name: str = Field(title="中转站名称")
+    throughput: float = Field(title="处理量")
+    count: int = Field(title="进场车次")
