8f86031dd7eee1f3b4f1709780acb741a178e9fd
zhuangbaojun <zhuangbaojun@ztbory.com>
2022-07-01 22:33:57 +0800

    fix: 添加中转站名称字段

--- recycle/schemas/inbound.py
+++ recycle/schemas/inbound.py
@@ -37,6 +37,7 @@ class InboundRecordIn(InboundRecordBase):
 
 class InboundRecordOut(InboundRecordBase):
     station_id: str = Field(title="中转站id")
+    station_name: str = Field(default=None, title="中转站名称")
     carrier_id: str = Field(None, title="运输单位（清运公司）id")
     id: str
 
